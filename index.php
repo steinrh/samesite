80556d1074403383bce6299bc4b8f7b5cca52105
Gabriel Ferraz Stein <gferrazs@redhat.com>
2020-09-01 14:54:05 +0200

    fixes setcookie

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 
-// Set a same-site cookie for first-party contexts
-setcookie('cookie1', 'value1', ['samesite' => 'Lax']);
-// Set a cross-site cookie for third-party contexts
 setcookie('cookie2', 'value2', ['samesite' => 'None', 'secure' => true]);
 
 ?>
