2b6aae45f8d197b8f30723196fedd034226b9711
Gabriel Ferraz Stein <gferrazs@redhat.com>
2020-09-01 16:09:09 +0200

    Changes cookie name

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo 'Set Samesite';
-header("Set-cookie: mycookie1=value; path=/; HttpOnly; SameSite=None; Secure', true");
+header("Set-cookie: mycookie2=value; path=/; HttpOnly; SameSite=None; Secure', true");
 
 ?>
