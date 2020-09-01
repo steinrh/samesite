316187e6e37f8c1ca54728cc0baa71c95b39c190
Gabriel Ferraz Stein <gferrazs@redhat.com>
2020-09-01 16:02:45 +0200

    Set Samesite secure == true

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo 'Set Samesite';
-header("Set-cookie: mycookie1=value; path=/; HttpOnly; SameSite=None; Secure', false");
+header("Set-cookie: mycookie1=value; path=/; HttpOnly; SameSite=None; Secure', true");
 
 ?>
